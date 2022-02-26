d170705dbc4f3d04eada671772f09285da02fdac
kkenji-rmg <79568046+kkenji-rmg@users.noreply.github.com>
2022-02-26 17:34:37 +0900

    各ブログ・ブログ投稿ページ

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -33,7 +33,33 @@ App::uses('PostsController', 'Controller');
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
 
+    // 一覧ページ
     public function index() {
         $this->set('posts', $this->Post->find('all'));
+    }
+
+    // 各ブログページ
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+    }
+
+    // ブログ投稿ページ
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Flash->success(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Flash->error(__('Unable to add your post.'));
+        }
     }
 }
